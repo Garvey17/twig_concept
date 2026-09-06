183f8291dcec9fdf94e36b5efa73050053101764
garvey17 <adebolaoluwatowo@gmail.com>
2026-09-06 19:50:09 +0100

    diff_parser completed

--- backend/app/github/diff_parser.py
+++ backend/app/github/diff_parser.py
@@ -1,4 +1,4 @@
-
+import unidiff
 from pydantic import BaseModel
 from typing import Literal
 
@@ -22,10 +22,45 @@ to_be_excluded = [
 ]
 
 def check_excluded_file(file_name: str) -> bool:
-    for file_format in to_be_excluded:
-        if file_name.startswith(file_format) or file_name.endswith(file_format):
+    for pattern in to_be_excluded:
+        if pattern.endswith("/"):
+            if pattern in file_name:
+                return True
+        elif file_name.endswith(pattern):
             return True
-
     return False
 
+def diff_parser(raw_diff: str) -> list[FileChange]:
+    patched_files = unidiff.PatchSet(raw_diff)
 
+    changed_files = []
+
+    for patch_file in patched_files:
+        if patch_file.is_binary_file or check_excluded_file(patch_file.path):
+            continue
+        hunks = []
+        for hunk in patch_file:
+            for line in hunk:
+                if line.is_added:
+                    line_no = line.target_line_no
+
+                    hunks.append(HunkLine(
+                        line_number=line_no,
+                        content=line.value.rstrip("\n"),
+                        type="addition"
+                    ))
+                elif line.is_context:
+                    line_no = line.target_line_no
+
+                    hunks.append(HunkLine(
+                        line_number=line_no,
+                        content=line.value.rstrip("\n"),
+                        type= "context"
+                    ))
+        if not hunks:
+            continue            
+        changed_files.append(FileChange(
+            filename=patch_file.path,
+            hunks=hunks
+        ))
+    return changed_files
